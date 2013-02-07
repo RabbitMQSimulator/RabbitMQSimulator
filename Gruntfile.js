c30e23d8b877390d7862b3e36ff8d87ccea08a78
Alvaro Videla <videlalvaro@gmail.com>
2013-02-07 23:31:27 +0100

    starts using grunt for managing js files

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            js: {
+                src: [
+                    'components/*.js'
+                ],
+                dest: 'web/js/scripts.js'
+            },
+            processing: {
+                src: [
+                    '*.pde'
+                ],
+                dest: 'web/js/Simulator.pde'
+            }
+        },
+        watch: {
+            files: ['components/*.js', '*.pde'],
+            tasks: 'default'
+        }
+    });
+
+    grunt.registerTask('default', ['concat']);
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+}; 
